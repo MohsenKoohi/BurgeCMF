590bae8308a85988743d848207f8d9a073a52a39
Mohsen Koohi <koohi@burge.ir>
2015-10-13 16:37:54 +0330

    font-size of visiting counter page in admin env

--- Web/application/views/en/admin/hit_counter.php
+++ Web/application/views/en/admin/hit_counter.php
@@ -1,4 +1,15 @@
 <div class="main">
+	<style type="text/css">
+		label.url
+		{
+			margin:0 20px;
+		}
+
+		.row.even-odd-bg span
+		{
+			font-size: .9em;
+		}
+	</style>
 	<div class="container">
 		<h1>{visiting_counter_text}</h1>
 
@@ -9,19 +20,19 @@
 						<label><?php echo $i++;?></label>
 					</div>
 					<div class="two columns">
-						{url_text}
-						<label><?php echo $counter['url'];?></label>
+						<span>{url_text}</span>
+						<label class="ltr eng url"><?php echo $counter['url'];?></label>
 					</div>
 					<div class="two columns">
-						{monthly_visit_text}
+						<span>{monthly_visit_text}</span>
 						<label><?php echo $counter['month_count']?></label>
 					</div>
 					<div class="two columns">
-						{yearly_visit_text}
+						<span>{yearly_visit_text}</span>
 						<label><?php echo $counter['year_count']?></label>
 					</div>
 					<div class="two columns">
-						{total_visit_text}
+						<span>{total_visit_text}</span>
 						<label><?php echo $counter['total_count']?></label>
 					</div>
 				</div>
