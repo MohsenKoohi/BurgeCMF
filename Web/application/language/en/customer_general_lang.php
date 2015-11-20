1ede72d3f9a13d1baf5e406a9f05f2919ae77eaa
Mohsen Koohi <koohi@burge.ir>
2015-11-20 23:08:37 +0330

    small corrections

--- Web/application/language/en/customer_general_lang.php
+++ Web/application/language/en/customer_general_lang.php
@@ -1,5 +1,5 @@
 <?php 
 
 $lang['header_title']="BurgeCMF";
-$lang['header_keywords']="BurgeCMF, Open-Source MVC Frame work, Burge Content Management Framework";
+$lang['header_keywords']="BurgeCMF, Burge Content Management Framework, Open-Source MVC Framework";
 $lang['header_description']="";
--- Web/application/language/fa/customer_general_lang.php
+++ Web/application/language/fa/customer_general_lang.php
@@ -1,5 +1,5 @@
 <?php 
 
 $lang['header_title']="فریمورک مدیرت محتوای برگه";
-$lang['header_keywords']="BurgeCMF, Open-Source MVC Frame work, Burge Content Management Framework, فریمورک مدیریت محتوای برگه, برگه";
+$lang['header_keywords']="BurgeCMF, Burge Content Management Framework, Open-Source MVC Framework, فریمورک مدیریت محتوای برگه, برگه";
 $lang['header_description']="";
