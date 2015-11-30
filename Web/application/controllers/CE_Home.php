d9996157b15505176c14fd37fca4152f8a2b67b4
Mohsen Koohi <koohi@burge.ir>
2015-11-30 16:40:18 +0330

    link correction in home pagE

--- Web/application/controllers/CE_Home.php
+++ Web/application/controllers/CE_Home.php
@@ -14,14 +14,13 @@ class CE_Home extends Burge_CMF_Controller {
 		
 		$this->lang->load('customer_home',$this->selected_lang);	
 
-		$this->data['lang_pages']=get_lang_pages(get_link("home_link",TRUE));
+		$this->data['lang_pages']=get_lang_pages(get_link("home_url",TRUE));
 		
 		$this->data['header_title'].=$this->lang->line("header_title");
 		$this->data['header_description'].=$this->lang->line("header_description");
 		$this->data['header_keywords'].=$this->lang->line("header_keywords");
 
-		$this->data['header_canonical_url']=get_link("home_link");
-		
+		$this->data['header_canonical_url']=get_link("home_url");
 
 		$this->send_customer_output("home");
 
