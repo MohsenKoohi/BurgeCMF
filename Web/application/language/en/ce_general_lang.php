3dfd3dea365c4a6c10b6452a97803c245ccc947e
Mohsen Koohi <koohi@burge.ir>
2016-02-21 23:37:12 +0330

    read more for each post

--- Web/application/language/en/ce_general_lang.php
+++ Web/application/language/en/ce_general_lang.php
@@ -1,6 +1,9 @@
 <?php 
+
 $lang['comma']=",";
 $lang['header_separator']=" | ";
 $lang['header_title']="BurgeCMF";
 $lang['header_meta_keywords']="BurgeCMF, Burge Content Management Framework, Open-Source MVC CMF";
 $lang['header_meta_description']="";
+
+$lang['read_more']="More ...";
--- Web/application/views/en/customer/home.php
+++ Web/application/views/en/customer/home.php
@@ -7,23 +7,25 @@
 			{
 		?>
 				<div class="row">
-					
-						<div class="twelve columns">
-							<a href="<?php echo get_customer_post_details_link($post['post_id'],$post['pc_title']);?>" >
-								<h2><?php echo $post['pc_title'];?></h2>
-								<img src="<?php echo $post['pc_image'];?>"/>
-								<br>
-								<?php 
-									$content=$post['pc_content'];
-									$content=preg_replace("/\s*<br\s*\/?>\s*/","\n",$content);
-									$content=str_replace("&nbsp;"," ", $content);
-									$content=strip_tags($content);
-									$content=mb_substr($content,0,100);								
-									$content=preg_replace("/(\s*\n+\s*)+/", "<br/>", $content);									
-									echo $content;
-								?>							
-							</a>
-						</div>
+					<div class="twelve columns">
+						<a href="<?php echo get_customer_post_details_link($post['post_id'],$post['pc_title']);?>" >
+							<h2><?php echo $post['pc_title'];?></h2>
+							<img src="<?php echo $post['pc_image'];?>"/>
+							<br>
+							<?php 
+								$content=$post['pc_content'];
+								$content=preg_replace("/\s*<br\s*\/?>\s*/","\n",$content);
+								$content=str_replace("&nbsp;"," ", $content);
+								$content=strip_tags($content);
+								$content=mb_substr($content,0,100);								
+								$content=preg_replace("/(\s*\n+\s*)+/", "<br/>", $content);
+								
+								echo $content."...";
+							?>	
+							<br>
+							<div class="read-more">{read_more_text}</div>
+						</a>
+					</div>
 					
 				</div>
 		<?php
