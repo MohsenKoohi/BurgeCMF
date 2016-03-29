3216cb31155f54a2c9b6495ff9d5d8a5f792eaf5
Mohsen Koohi <koohi@burge.ir>
2016-03-29 21:02:21 +0430

    style correction

--- Web/application/views/en/admin/login.php
+++ Web/application/views/en/admin/login.php
@@ -4,15 +4,15 @@
 		<?php echo form_open(get_link("admin_login"),array()); ?>
 			<div class="col12 columns mrg-btn-20">
 				<label>{email_text}</label>
-				<input class="ltr full-width" type="text" name="email" />
+				<input class="lang-en full-width" type="text" name="email" />
 			</div>
 			<div class="col12 columns mrg-btn-20">
 				<label>{password_text}</label>
-				<input class="ltr full-width" type="password" name="pass"/>
+				<input class="lang-en full-width" type="password" name="pass"/>
 			</div>
 			<div class="col12 columns mrg-btn-40">
 				<labeL>{captcha}</label>
-				<input class="ltr full-width" type="text" name="captcha" />
+				<input class="lang-en full-width" type="text" name="captcha" />
 			</div>
 		
 			<div class="12 columns">				
