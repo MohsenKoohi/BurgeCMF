43d435ef2d784ae3f370b7c47fa10a879df0f059
Mohsen Koohi <koohi@burge.ir>
2016-04-01 19:15:51 +0430

    showing image post in the post page

--- Web/application/views/en/customer/post.php
+++ Web/application/views/en/customer/post.php
@@ -3,6 +3,11 @@
 		<h1><?php echo $post_info['pc_title'];?></h1>
 		<div class="post-date"><?php echo str_replace("-","/",$post_info['post_date']);?></div>
 		<div class="row">
+			<?php if($post_info['pc_image']) { ?>
+				<div class="post-img" style="background-image:url('<?php echo $post_info['pc_image'];?>')">
+				</div>
+				<br><br>
+			<?php } ?>
 			<div class="twelve columns post-content">
 				<?php echo $post_info['pc_content'] ?>
 			</div>
