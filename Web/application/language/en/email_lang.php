51704c324ffc5128aad0c3c168bd57b50b94489a
Mohsen Koohi <koohi@burge.ir>
2016-04-03 12:06:39 +0430

    customer login, singup, and dashboard

--- Web/application/language/en/email_lang.php
+++ Web/application/language/en/email_lang.php
@@ -9,8 +9,8 @@ $lang['email_template']='
 	</head>
 	<body style="direction:ltr;font-family:Times new roman, Arial;">
 		<div style=";height:150px;display:block;text-align:center;direction:ltr;">
-			 <a title="" alt="" href="'.HOME_URL.'"><img src="'.IMAGES_URL.'/logo-text.png"  style="height:130px" ></a>
-		 <a title="" alt="" href="'.HOME_URL.'"><img src="'.IMAGES_URL.'/logo-notext.png"  style="height:130px"></a>		    
+			 <a title="" alt="" href="'.HOME_URL.'"><img src="'.IMAGES_URL.'/logo-text.png"  style="max-height:130px" ></a>
+		 <a title="" alt="" href="'.HOME_URL.'"><img src="'.IMAGES_URL.'/logo-notext.png"  style="max-height:130px"></a>		    
 		</div>
 		<div class="main" style="direction:ltr;font-size:1.3em;font-family:Times new roman,Arial;line-height:1.8em;background:white;min-height:200px">
 		$content
--- Web/application/language/fa/email_lang.php
+++ Web/application/language/fa/email_lang.php
@@ -9,8 +9,8 @@ $lang['email_template']='
 	</head>
 	<body style="direction:rtl;font-family:b koodak, koodak,b mitra, mitra, tahoma;">
 		<div style=";height:150px;display:block;text-align:center;direction:rtl;">
-			 <a title="" alt="" href="'.HOME_URL.'"><img src="'.IMAGES_URL.'/logo-text-fa.png"  style="height:130px" ></a>
-		 <a title="" alt="" href="'.HOME_URL.'"><img src="'.IMAGES_URL.'/logo-notext.png"  style="height:130px"></a>		    
+			 <a title="" alt="" href="'.HOME_URL.'"><img src="'.IMAGES_URL.'/logo-text-fa.png"  style="max-height:130px" ></a>
+		 <a title="" alt="" href="'.HOME_URL.'"><img src="'.IMAGES_URL.'/logo-notext.png"  style="max-height:130px"></a>		    
 		</div>
 		<div class="main" style="direction:rtl;font-size:1.3em;font-family:b koodak, koodak, b mitra, mitra, tahoma;text-align:justify;line-height:1.8em;background:white;min-height:200px">
 		$content
