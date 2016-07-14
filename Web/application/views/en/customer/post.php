cf30d5faed162e9c68c40e4053db635a48bb82e9
Mohsen Koohi <koohi@burge.ir>
2016-07-14 20:51:32 +0430

    Post image correction

--- Web/application/views/en/customer/post.php
+++ Web/application/views/en/customer/post.php
@@ -17,8 +17,10 @@
 		<div class="post-date"><?php echo str_replace("-","/",$post_info['post_date']);?></div>
 		<div class="row">
 			<?php if($post_info['pc_image']) { ?>
-				<div class="post-img lazy-load" data-ll-type="background-image"
-				 data-ll-url="<?php echo $post_info['pc_image'];?>">
+				<div class="post-img">
+					<img class="lazy-load" data-ll-type="src"
+						data-ll-url="<?php echo $post_info['pc_image'];?>"
+					/>
 				</div>
 				<br><br>
 			<?php } ?>
