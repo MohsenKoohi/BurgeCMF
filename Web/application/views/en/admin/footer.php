0ed9b9078a540db30c6a3e12060ba0ac7d44e212
Mohsen Koohi <koohi@burge.ir>
2016-08-02 23:05:27 +0430

    style correction for .content when we have a message

--- Web/application/views/en/admin/footer.php
+++ Web/application/views/en/admin/footer.php
@@ -1,5 +1,10 @@
 		</div>
 	</div>
 </div>
+<div class="footer">
+	<a href="http://burge.ir/category-3/BurgeCMF" target="_blank">
+		<img src="{images_url}/logo-text-en.png"  title="Burge ATS Homepage"/>
+	</a>
+</div>
 </body>
 </html>
