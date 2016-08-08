0a689aa0aa7f72f565ab7cd77908d344ad2719fe
Mohsen Koohi <koohi@burge.ir>
2016-08-08 15:08:08 +0430

    Adding search module, a module will be used for search through the site, it invokes 'search_module' method of each module

---
+++ Web/application/models/Search_manager_model.php
@@ -0,0 +1,48 @@
+<?php
+class Search_manager_model extends CI_Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+		
+		return;
+	}
+
+	public function install()
+	{
+		
+		return;
+	}
+
+	public function uninstall()
+	{
+
+		return;
+	}		
+
+	public function search($text,$filter)
+	{
+		$this->load->model("module_manager_model");
+		$modules=$this->module_manager_model->get_all_modules_info($this->selected_lang);
+
+		$results=array();
+
+		foreach ($modules as $module)
+		{
+			$name=$module['name'];
+			$model_name=$module['model'];
+			if(!$model_name)
+				continue;
+		
+			$this->load->model($model_name."_model");
+			$model=$this->{$model_name."_model"};
+
+			if(!method_exists($model, "search_module"))
+				continue;
+
+			$results[$name]=$model->{"search_module"}($text, $search);
+		}
+
+		return $results;
+	}
+}
