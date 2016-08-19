9888949e0aa163600b0de2e341e0319b5668a327
Mohsen Koohi <koohi@burge.ir>
2016-08-19 18:34:15 +0430

    admin footer link correction

--- Web/application/views/en/admin/footer.php
+++ Web/application/views/en/admin/footer.php
@@ -3,7 +3,7 @@
 </div>
 <div class="footer">
 	<a href="http://burge.ir/category-3/BurgeCMF" target="_blank">
-		<img src="{images_url}/logo-text-en.png"  title="Burge ATS Homepage"/>
+		<img src="{images_url}/logo-text-en.png"  title="BurgeCMF Homepage"/>
 	</a>
 </div>
 </body>
