9e53cb181440f3d901cd2deb27f1774f4b4c2581
Mohsen Koohi <koohi@burge.ir>
2016-12-18 08:59:25 +0330

    add table toolbar to tinyMCE

--- Web/application/views/en/admin/category_details.php
+++ Web/application/views/en/admin/category_details.php
@@ -374,10 +374,10 @@
 							+"Verdana=verdana,geneva;"
 							+"Webdings=webdings;"
 							+"Wingdings=wingdings,zapf dingbats";
-						var tinyMCEPlugins="directionality textcolor link image hr emoticons2 lineheight colorpicker media";
+						var tinyMCEPlugins="directionality textcolor link image hr emoticons2 lineheight colorpicker media table";
 						var tinyMCEToolbar=[
 						   "link image media hr bold italic underline strikethrough alignleft aligncenter alignright alignjustify styleselect formatselect fontselect fontsizeselect  emoticons2",
-						   "cut copy paste bullist numlist outdent indent forecolor backcolor removeformat  ltr rtl lineheightselect "
+						   "cut copy paste bullist numlist outdent indent forecolor backcolor removeformat  ltr rtl lineheightselect table"
 						];
 
 						
