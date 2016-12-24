eda5a1d99b82d717b4561b532a384f40fdc50641
Mohsen Koohi <koohi@burge.ir>
2016-12-25 01:07:54 +0330

    user group english lang files

--- Web/application/language/en/ae_user_lang.php
+++ Web/application/language/en/ae_user_lang.php
@@ -14,3 +14,12 @@ $lang['added_successfully']="New user added successfully.";
 $lang['repeated_email']="This email has been added previously.";
 $lang['modfied_successfully']="Changes have been done successfully.";
 
+$lang['user_groups_list']="User groups list";
+$lang['add_user_group']="Add user group";
+$lang['ug_added_successfully']="New user group added successfully.";
+$lang['user_group']="User group";
+$lang['']="";
+$lang['']="";
+$lang['']="";
+$lang['']="";
+$lang['']="";
