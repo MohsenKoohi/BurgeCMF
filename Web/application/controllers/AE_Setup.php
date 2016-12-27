75e09b1f71b2c530aa88926e24adf42cc98de258
Mohsen Koohi <koohi@burge.ir>
2016-12-27 19:05:34 +0330

    AE_Setup changes to comply with current chagnes

--- Web/application/controllers/AE_Setup.php
+++ Web/application/controllers/AE_Setup.php
@@ -102,7 +102,7 @@ class AE_Setup extends CI_Controller {
 		foreach($modules_info as $md)
 			$modules[]=$md['module_id'];
 		$this->load->model("access_manager_model");
-		$this->access_manager_model->set_allowed_modules_for_user($user->get_id(),$modules);
+		$this->access_manager_model->set_modules(-$user->get_id(),$modules);
 
 		return;
 	}
